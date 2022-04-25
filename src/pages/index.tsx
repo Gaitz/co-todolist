52686915cba742e6b97c02711048a2373d5a1c55
Cheng PoHuai <ragnarokcute@gmail.com>
2022-04-25 23:07:31 +0800

    feat: add Redux

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '@shared/styles/Home.module.css'
 import UserAuthentication from '@userAuthentication/view/UserAuthentication'
 
 const Home: NextPage = () => {
